cdeb248f31a690d35e295f9482816e2d055a8e0e
graf6672 <glomozdai@mail.ru>
2026-07-19 10:41:01 +0700

    Add Account domain entity

--- FinTracker.Domain/Entities/Account.cs
+++ FinTracker.Domain/Entities/Account.cs
@@ -1,10 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FinTracker.Domain.Enums;
 
 namespace FinTracker.Domain.Entities
 {
     public class Account
     {
+        public int Id { get; private set; }
+
+        public int UserId { get; private set; }
+        public User User { get; private set; }
+
+        public string Name { get; private set; }
+        public decimal Balance { get; private set; }
+        public AccountStatus Status { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        public ICollection<Transaction> Transactions { get; private set; } = new List<Transaction>();
+
+        public Account(User user, string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name cannot be empty.");
+            }
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            Name = name;
+            User = user;
+            UserId = user.Id;
+            CreatedAt = DateTime.UtcNow;
+            Status = AccountStatus.Active;
+        }
+
+        public void Rename(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("New name cannot be empty.");
+            }
+            if (name == Name)
+            {
+                throw new ArgumentException("New name cannot be same as old name.");
+            }
+
+            Name = name;
+        }
+        public void Close()
+        {
+            if (Status == AccountStatus.Closed)
+            {
+                throw new ArgumentException("The account is already closed");
+            }
+
+            Status = AccountStatus.Closed;
+        }
     }
 }
---
+++ FinTracker.Domain/Enums/AccountStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinTracker.Domain.Enums
+{
+    public enum AccountStatus
+    {
+        Active,
+        Closed
+    }
+}
