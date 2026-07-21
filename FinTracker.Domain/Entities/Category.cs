fd09a52e07b0ba0b943aa0839836c01915f499de
graf6672 <glomozdai@mail.ru>
2026-07-21 10:40:19 +0700

    Add Category domain entity

--- FinTracker.Domain/Entities/Category.cs
+++ FinTracker.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FinTracker.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,47 @@ namespace FinTracker.Domain.Entities
 {
     public class Category
     {
+        public int Id { get; private set; }
+
+        public int UserId { get; private set; }
+        public User User { get; private set; }
+
+        public string Name { get; private set; }
+
+        public CategoryType Type { get; private set; }
+
+        public ICollection<Transaction> Transactions { get; private set; } = new List<Transaction>();
+
+        public Category(User user, string name, CategoryType type)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name cannot be empty.", nameof(name));
+            }
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            User = user;
+            UserId = user.Id;
+            Name = name;
+            Type = type;
+        }
+
+        public void Rename(string name)
+        {
+            name.Trim();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("New name cannot be empty.", nameof(name));
+            }
+            if (Name == name)
+            {
+                throw new ArgumentException("New name cannot be same as old name.", nameof(name));
+            }
+
+            Name = name;
+        }
     }
 }
--- FinTracker.Domain/Enums/CategoryType.cs
+++ FinTracker.Domain/Enums/CategoryType.cs
@@ -4,7 +4,9 @@ using System.Text;
 
 namespace FinTracker.Domain.Enums
 {
-    internal class CategoryType
+    public enum CategoryType
     {
+        Income,
+        Expense
     }
 }
